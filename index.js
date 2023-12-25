66d75a1fe7447424eb63b41ea43b4a0e7854ace1
GoodPointt <dev6012@gmail.com>
2023-12-25 12:55:35 +0200

    render predeploy commit

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 require('dotenv').config({ path: './config/.env' });
 
+const schedule = require('node-schedule');
+const axios = require('axios');
 const mongoose = require('mongoose');
 const { setupBot } = require('./bot');
 
 (async function () {
   try {
     await mongoose.connect(process.env.DB_TOKEN, {
-      // useUnifiedTopology: true,
-      // useNewUrlParse: true,
       dbName: 'db-weatherbot',
     });
 
     console.log('🌐 Database connect success');
 
     await setupBot().launch();
-
-    // const bot = setupBot();
-    // await bot.launch()
   } catch (error) {
     console.log('❌ ERROR!!', error);
   }
 })();
+
+const job = schedule.scheduleJob('*/14 * * * *', async () => {
+  try {
+    const response = await axios.get(REQ_URL);
+    if (response.status === 200) {
+      console.log('✅ GET request to API successful');
+    } else {
+      console.error(
+        '❌ GET request to API failed:',
+        response.status,
+        response.statusText
+      );
+    }
+  } catch (error) {
+    console.error('❌ Error making GET request:', error.message);
+  }
+});
